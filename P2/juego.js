c6800cbd2656de44899c23fe44903331b183399b
Cristal <cristalcamposabad@gmail.com>
2022-03-29 16:43:47 +0200

    [P2] Implementación de Patterns
    Incompleta. No funciona.

--- P2/juego.js
+++ P2/juego.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById("campo");
 var ctx = canvas.getContext("2d");
 
 // Medidas
-var largo_pala = 50;
+var largo_pala = 100;
 var line_dash = [5, 3];
 var radio_centro = 50;
 var radio_bola = 10;
@@ -13,14 +13,17 @@ var height = canvas.height;
 // Posiciones
 var pos_ini1 = [0, height/2 - largo_pala/2];
 var pos_ini2 = [width, height/2 - largo_pala/2];
-var pos1 = [0, 0];
-var pos2 = [0, 0];
+var pos1 = pos_ini1;
+var pos2 = pos_ini2;
 var centro = [width/2, height/2]; // Centro
 
 // Imágenes
-var pala1 = new Image();
-pala1.src= "pala1.png";
-var pala2 = "pala2.png";
+var imgPala1 = new Image();
+imgPala1.src= "./pala1.png";
+var patPala1 = ctx.createPattern(imgPala1, "repeat");
+var imgPala2 = new Image();
+imgPala2.src= "./pala2.png";
+var patPala2 = ctx.createPattern(imgPala2, "repeat");
 
 window.onload = start();
 
@@ -64,6 +67,15 @@ function start() {
 
     // Pala 1
     ctx.beginPath();
-    ctx.drawImage(pala1, pos1[0], pos1[1], 20, largo_pala);
+    ctx.fillStyle = patPala1;
+    ctx.rect(pos1[0], pos1[1], 20, largo_pala);
+    ctx.fill();
+    ctx.stroke();
+
+    // Pala 2
+    ctx.beginPath();
+    ctx.fillStyle = patPala2;
+    ctx.rect(pos2[0] - 20, pos2[1], 20, largo_pala);
+    ctx.fill();
     ctx.stroke();
 }
