7c12a43544ea4bde5c1ee9ec66fab78a828e930b
Camilo <caena@alumni.uv.es>
2022-03-29 19:16:18 +0200

    Recuperando el campo

--- P2/juego.js
+++ P2/juego.js
@@ -1,33 +1,144 @@
+class Vector {
+
+    constructor(x, y){
+        this._x = x;
+        this._y = y;
+    }
+
+    normalize() {
+        let norma = Math.sqrt(Math.pow(_x, 2) + Math.pow(_y, 2)); //módulo
+        _x /= norma;
+        _y /= norma; 
+    }
+
+    add(x, y) {
+        this._x += x;
+        this._y += y;
+    }
+
+    sub(x, y) {
+        this._x -= x;
+        this._y -= y;
+    }
+
+    mult(n){
+        _x *= n;
+        _y *= n;
+    }
+
+    copy(v){
+        this._x = v.x;
+        this._y = v.y;
+    }
+
+    // Setters _y Getters
+    get x(){
+        return this._x;
+    }
+    get y(){
+        return this._y;
+    }
+    set x(a){
+        this._x = a;
+    }
+    set y(a){
+        this._y = a;
+    }
+}
+
 // Lienzo
-var canvas = document.getElementById("campo");
-var ctx = canvas.getContext("2d");
+var canvas;
+var ctx;
 
 // Medidas
-var largo_pala = 100;
-var line_dash = [5, 3];
-var radio_centro = 50;
-var radio_bola = 10;
-var width = canvas.width;
-var height = canvas.height;
+var largo_pala;
+var line_dash;
+var radio_centro;
+var radio_bola;
+var width;
+var height;
 
 // Posiciones
-var pos_ini1 = [0, height/2 - largo_pala/2];
-var pos_ini2 = [width, height/2 - largo_pala/2];
-var pos1 = pos_ini1;
-var pos2 = pos_ini2;
-var centro = [width/2, height/2]; // Centro
+var pos_ini_pala1;
+var pos_ini_pala2;
+var pos_pala1;
+var pos_pala2;
+var centro_campo; // Centro
 
 // Imágenes
-var imgPala1 = new Image();
-imgPala1.src= "./pala1.png";
-var patPala1 = ctx.createPattern(imgPala1, "repeat");
-var imgPala2 = new Image();
-imgPala2.src= "./pala2.png";
-var patPala2 = ctx.createPattern(imgPala2, "repeat");
+var imgPala1;
+var patPala1;
+var imgPala2;
+var patPala2;
+
+// Bola
+var grad_bola;
+var pos_bola;
+var deltaX; //incremento
+var deltaY;
+var dir_bola;
+var salida_bola;
+
 
 window.onload = start();
 
 function start() {
+    // Lienzo
+    canvas = document.getElementById("campo");
+    ctx = canvas.getContext("2d");
+
+    // Medidas
+    largo_pala = 100;
+    line_dash = [5, 3];
+    radio_centro = 50;
+    radio_bola = 10;
+    width = canvas.width;
+    height = canvas.height;
+
+    // Posiciones
+    pos_ini_pala1 = new Vector(0, height/2 - largo_pala/2);
+    pos_ini_pala2 = new Vector(width, height/2 - largo_pala/2);
+    //pos_pala1.copy(pos_ini_pala1);
+    //pos_pala2.copy(pos_ini_pala2);
+    pos_pala1 = pos_ini_pala1;
+    pos_pala2 = pos_ini_pala2;
+    centro_campo = new Vector(width/2, height/2); // Centro
+
+    // Bola
+    pos_bola = centro_campo;
+    salida_bola = (Math.floor(Math.random()*10)%4);
+    switch(salida_bola) {
+        case 0:
+            dir_bola = new Vector(Math.cos(toRad(35)), Math.sin(toRad(35)));
+            break;
+        case 1:
+            dir_bola = new Vector(Math.cos(toRad(145)), Math.sin(toRad(145)));
+            break;
+        case 2:
+            dir_bola = new Vector(Math.cos(toRad(215)), Math.sin(toRad(215)));
+            break;
+        case 3:
+        default:
+            dir_bola = new Vector(Math.cos(toRad(325)), Math.sin(toRad(325)));
+            break;
+    }
+    console.log(dir_bola.x);
+
+    // Imágenes
+    imgPala1 = new Image();
+    imgPala1.src= "./pala1.png";
+    patPala1 = ctx.createPattern(imgPala1, "repeat");
+    imgPala2 = new Image();
+    imgPala2.src= "./pala2.png";
+    patPala2 = ctx.createPattern(imgPala2, "repeat");
+    
+
+    setInterval(draw(), 100);
+}
+
+function draw() {
+
+
     // Fondo
     ctx.beginPath();
     ctx.fillStyle = "darkseagreen";
@@ -50,32 +161,40 @@ function start() {
     ctx.setLineDash(line_dash);
     ctx.lineCap = "round";
     ctx.strokeStyle = "white";
-    ctx.arc(centro[0], centro[1], radio_centro, 0, 2*Math.PI);
+    ctx.arc(centro_campo.x, centro_campo.y, radio_centro, 0, 2 * Math.PI);
     ctx.stroke();
 
     // Bola
     ctx.beginPath();
     ctx.setLineDash([]);
     ctx.strokeStyle = "indigo";
-    var grad_bola = ctx.createRadialGradient(centro[0], centro[1], radio_bola, centro[0], centro[1], radio_bola/5);
+    grad_bola = ctx.createRadialGradient(centro_campo[0], centro_campo[1], radio_bola, centro_campo[0], centro_campo[1], radio_bola/5);
     grad_bola.addColorStop(0, "indigo");
     grad_bola.addColorStop(1, "mediumvioletred");
     ctx.fillStyle = grad_bola;
-    ctx.arc(centro[0], centro[1], radio_bola, 0, 2*Math.PI);
+    ctx.arc(centro_campo[0], centro_campo[1], radio_bola, 0, 2*Math.PI);
     ctx.fill();
     ctx.stroke();
 
     // Pala 1
     ctx.beginPath();
-    ctx.fillStyle = patPala1;
-    ctx.rect(pos1[0], pos1[1], 20, largo_pala);
-    ctx.fill();
+    //ct_x.fillStyle = patPala1;
+    ctx.fillRect(pos_pala1[0], pos_pala1[1], 20, largo_pala);
+    //ct_x.fill();
     ctx.stroke();
 
     // Pala 2
     ctx.beginPath();
-    ctx.fillStyle = patPala2;
-    ctx.rect(pos2[0] - 20, pos2[1], 20, largo_pala);
-    ctx.fill();
+    //ct_x.fillStyle = patPala2;
+    ctx.fillRect(pos_pala2[0] - 20, pos_pala2[1], 20, largo_pala);
+    //ct_x.fill();
     ctx.stroke();
+}
+
+function moverBola() {
+    pos_bola = [pos_bola[0]+deltaX, pos_bola[0]+deltaY];
+}
+
+function toRad(grados) {
+    return (grados * 2 * Math.PI / 360);
 }
